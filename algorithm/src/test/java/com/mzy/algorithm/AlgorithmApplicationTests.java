274d75080d1c188219fceeaef37e8a629fdd9edb
米照雅 <mizy@wdcloud.cc>
2020-09-04 18:06:15 +0800

    堆

---
+++ algorithm/src/test/java/com/mzy/algorithm/AlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzy.algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
