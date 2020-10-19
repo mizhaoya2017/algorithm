be3116ede04dd4a44ab7fcc8d3d837cc369d879e
米照雅 <mizy@wdcloud.cc>
2020-10-19 16:36:48 +0800

    算法第二日

--- algorithm/src/main/java/com/mzy/algorithm/mianshi/Solution_20201015.java
+++ algorithm/src/main/java/com/mzy/algorithm/mianshi/Solution_20201015.java
@@ -66,10 +66,38 @@ public class Solution_20201015 {
         return j-i-1;
     }
 
+    public static String longestPalindrome3(String s) {
+        int len = s.length();
+        int[][] a = new int[1000][1000];
+        int maxLen = 1;
+        int begin = 0;
+        for (int i = 0; i < len; i ++) {
+            a[i][i] = 1;
+        }
+        for (int j = 0; j < len; j ++) {
+            for (int i = 0; i < j; i ++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i < 3) {
+                        a[i][j] = 1;
+                    }else {
+                        a[i][j] = a[i + 1][j -1];
+                    }
+                } else {
+                    a[i][j] = 0;
+                }
+                if (a[i][j] == 1 && maxLen < j - i + 1) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
     public static void main(String[] args) {
-//        String s1 = longestPalindrome2("babad");
-//        String s2 = longestPalindrome2("cbbd");
-//        System.out.println(s1);
-//        System.out.println(s2);
+        String s1 = longestPalindrome3("babad");
+        String s2 = longestPalindrome3("cbbd");
+        System.out.println(s1);
+        System.out.println(s2);
     }
 }
