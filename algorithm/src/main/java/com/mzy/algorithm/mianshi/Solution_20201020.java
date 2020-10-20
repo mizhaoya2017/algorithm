29ad977cd38a1e3d3bff576daf761244ec861b7e
mizy <mizy@wdcloud.cc>
2020-10-20 23:16:56 +0800

    算法：字符串转换整数 (atoi)

--- algorithm/src/main/java/com/mzy/algorithm/mianshi/Solution_20201020.java
+++ algorithm/src/main/java/com/mzy/algorithm/mianshi/Solution_20201020.java
@@ -2,6 +2,7 @@ package com.mzy.algorithm.mianshi;
 
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 /**
@@ -11,20 +12,86 @@ import java.util.regex.Pattern;
  */
 public class Solution_20201020 {
     public static int myAtoi(String s) {
+        char[] x = new char[]{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+        ArrayList y = new ArrayList(10);
+        for (int i = 0; i < x.length; i ++) {
+            y.add(x[i]);
+        }
+        int res = 0;
         String trim = s.trim();
         int len = trim.length();
         boolean flag = true;
-        if (!StringUtils.isEmpty(trim) && trim.charAt(0) == '-') {
+        if (!"".equals(trim) && trim.charAt(0) == '-') {
             flag = false;
             len -= 1;
             trim = trim.substring(1);
+        } else if (!"".equals(trim) && trim.charAt(0) == '+') {
+            len -= 1;
+            trim = trim.substring(1);
+        } else if ("".equals(trim) || !y.contains(trim.charAt(0))) {
+            return 0;
         }
         for (int i = 0; i < len; i ++) {
-            if (Pattern.matches("",trim.charAt(i));
+            if (y.contains(trim.charAt(i))) {
+                int r = trim.charAt(i) - '0';
+                if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && r > 7)) {
+                    res = Integer.MAX_VALUE;
+                    return flag ? Integer.MAX_VALUE: Integer.MIN_VALUE;
+                }
+                res = res * 10 + Integer.parseInt(String.valueOf(trim.charAt(i)));
+            } else {
+                break;
+            }
         }
-        return 1;
+        if (!flag) {
+            res = -res;
+        }
+        return res;
+    }
+
+    public static int myAtoi2(String s) {
+        int res = 0;
+        int flag = 1;
+        int i = 0;
+        if (s.length()==0) return 0;
+        while(s.length() > i && ' '== s.charAt(i)) {i ++;}
+        if (s.length() > i && s.charAt(i) == '-') {
+            flag = 0;
+        }
+        if (s.length() > i && (s.charAt(i) == '+'|| s.charAt(i) == '-')) {
+            i ++;
+        }
+        while(i < s.length() && Character.isDigit(s.charAt(i))) {
+            int r = s.charAt(i) - '0';
+            if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE/10 && r > 7)) {
+                return flag > 0 ?Integer.MAX_VALUE: Integer.MIN_VALUE;
+            }
+            res = res * 10 + r;
+            i ++;
+        }
+        return flag>0?res:-res;
     }
 
     public static void main(String[] args) {
+        System.out.println(myAtoi2("42"));
+        System.out.println(myAtoi2("     -42"));
+        System.out.println(myAtoi2("4193 with words"));
+        System.out.println(myAtoi2("words and 987"));
+        System.out.println(myAtoi2("-91283472332"));
+        System.out.println(myAtoi2("+1"));
+        System.out.println(myAtoi2("2147483648"));
+        System.out.println(myAtoi2("2147483646"));
+        System.out.println(myAtoi2(""));
+        System.out.println(myAtoi2(" "));
+
+
+        System.out.println(myAtoi("42"));
+        System.out.println(myAtoi("     -42"));
+        System.out.println(myAtoi("4193 with words"));
+        System.out.println(myAtoi("words and 987"));
+        System.out.println(myAtoi("-91283472332"));
+        System.out.println(myAtoi("+1"));
+        System.out.println(myAtoi("2147483648"));
+        System.out.println(myAtoi("2147483646"));
     }
 }
