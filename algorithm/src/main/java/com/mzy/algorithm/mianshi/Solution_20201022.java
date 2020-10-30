26aa869b35c7183a0930880ee781ecb93cf77aba
mizy <mizy@wdcloud.cc>
2020-10-30 21:23:34 +0800

    1022

--- algorithm/src/main/java/com/mzy/algorithm/mianshi/Solution_20201022.java
+++ algorithm/src/main/java/com/mzy/algorithm/mianshi/Solution_20201022.java
@@ -14,15 +14,31 @@ public class Solution_20201022 {
         Map<Integer, Integer> map = new HashMap<>();
         int x = 0;
         for (int i = 0; i < height.length; i ++) {
-            if (isMax(height[i], map, x)) {
+            if (isMax(height[i], i, map, x)) {
+                x = getMax(height[i], i, map, x) 
             }
         }
         return 1;
     }
 
-    public static boolean isMax(int c, Map<Integer, Integer> map, int x) {
-
+    public static boolean isMax(int c, int i, Map<Integer, Integer> map, int x) {
+        for (Integer key: map.keySet()) {
+            Integer value = map.get(key);
+            if (Math.max(c, key) * (value - i) > x) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    public static int getMax(int c, int i, Map<Integer, Integer> map, int x) {
+        for (Integer key: map.keySet()) {
+            Integer value = map.get(key);
+            if (Math.max(c, key) * (value - i) > x) {
+                return value;
+            }
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
