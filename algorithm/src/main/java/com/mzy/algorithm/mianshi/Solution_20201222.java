bb51d863ec6a92a60b8e89d4f6cea49592a05fcd
mizy <mizy@wdcloud.cc>
2021-01-04 23:59:58 +0800

    两数相除

--- algorithm/src/main/java/com/mzy/algorithm/mianshi/Solution_20201222.java
+++ algorithm/src/main/java/com/mzy/algorithm/mianshi/Solution_20201222.java
@@ -1,5 +1,8 @@
 package com.mzy.algorithm.mianshi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author ：mizhaoya
  * @date ：2020/12/14 17:51
@@ -8,27 +11,51 @@ package com.mzy.algorithm.mianshi;
 public class Solution_20201222 {
 
     public static int divide(int dividend, int divisor) {
-        int i = 0;
-        int res = 0;
-
-        if(divisor > 0) {
-            while(res <= dividend) {
-                res += divisor;
-                i ++;
+        int INT_MAX = 0x7FFFFFFF;
+        int INT_MIN = 1 << 31;
+        int _dividend = dividend < 0? dividend: -dividend;
+        int _divisor = divisor < 0 ? divisor: -divisor;
+        int symbol = dividend ^ divisor >> 31;
+        List<Integer> times = getTimes(_dividend, _divisor, new ArrayList<>());
+        int output = 0;
+        for (int t: times) {
+            if (t == 31) {
+                if (symbol == 0) {
+                    return INT_MAX;
+                } else {
+                    return INT_MIN;
+                }
             }
-            return i - 1;
-        } else {
-            while(-res <= dividend) {
-                res += divisor;
-                i ++;
-            }
-            return -i + 1;
+            output += 1 << t;
         }
+        return symbol==0?output:-output;
 
     }
 
+    public static List<Integer> getTimes(int dividend, int divisor, List<Integer> times) {
+        if (dividend > divisor) {
+            return times;
+        }
+        int maxTime = 32;
+        int minTime = 0;
+        while(maxTime != minTime + 1) {
+            int mid = (minTime + maxTime) >> 1;
+            if (divisor < -1 << (31 - mid)) {
+                maxTime = mid;
+                continue;
+            }
+            if (divisor << mid < dividend) {
+                maxTime = mid;
+            } else {
+                minTime = mid;
+            }
+        }
+        times.add(minTime);
+        return getTimes(dividend - (divisor << minTime), divisor, times);
+    }
+
     public static void main(String[] args) {
-        System.out.println(divide(10,3));
-        System.out.println(divide(7,-3));
+        System.out.print(divide(10,3));
+        System.out.print(divide(7,-3));
     }
 }
