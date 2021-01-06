ce6c070004065dcb1e145e3c77c2bca8e5d15ea2
米照雅 <mizy@wdcloud.cc>
2021-01-06 18:30:18 +0800

    下一个排列

--- algorithm/src/main/java/com/mzy/algorithm/mianshi/Solution_20201223.java
+++ algorithm/src/main/java/com/mzy/algorithm/mianshi/Solution_20201223.java
@@ -7,12 +7,40 @@ package com.mzy.algorithm.mianshi;
  */
 public class Solution_20201223 {
 
-    public static void nextPermutation(int[] nums) {
+    public static int[] nextPermutation(int[] nums) {
+        if (isDesc(nums)) {
+            for (int i = 0; i < nums.length/2; i ++) {
+                int a = nums[i];
+                nums[i] = nums[nums.length - 1 - i];
+                nums[nums.length - 1 - i] = a;
+            }
+        } else {
+            for (int i = nums.length-1; i > 0; i --) {
+                if (nums[i] > nums[i - 1]) {
+                    int a = nums[i];
+                    nums[i] = nums[i - 1];
+                    nums[i - 1] = a;
+                    break;
+                }
+            }
+        }
+        return nums;
+    }
 
+    public static boolean isDesc(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i ++) {
+            if (nums[i] < nums[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(nextPermutation(new int[]{1,2,3}));
+        System.out.println(nextPermutation(new int[]{3,2,1}));
+        System.out.println(nextPermutation(new int[]{1,1,5}));
+        System.out.println(nextPermutation(new int[]{1}));
 
     }
 }
