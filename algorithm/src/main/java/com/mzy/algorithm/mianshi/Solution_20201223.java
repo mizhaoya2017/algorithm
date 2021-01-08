fee8d20f27c81070385343445c1047d0f868a278
米照雅 <mizy@wdcloud.cc>
2021-01-08 14:13:52 +0800

    下一个排列

--- algorithm/src/main/java/com/mzy/algorithm/mianshi/Solution_20201223.java
+++ algorithm/src/main/java/com/mzy/algorithm/mianshi/Solution_20201223.java
@@ -8,32 +8,34 @@ package com.mzy.algorithm.mianshi;
 public class Solution_20201223 {
 
     public static int[] nextPermutation(int[] nums) {
-        if (isDesc(nums)) {
-            for (int i = 0; i < nums.length/2; i ++) {
-                int a = nums[i];
-                nums[i] = nums[nums.length - 1 - i];
-                nums[nums.length - 1 - i] = a;
-            }
-        } else {
-            for (int i = nums.length-1; i > 0; i --) {
-                if (nums[i] > nums[i - 1]) {
-                    int a = nums[i];
-                    nums[i] = nums[i - 1];
-                    nums[i - 1] = a;
-                    break;
-                }
-            }
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i --;
         }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j > 0 && nums[i] >= nums[j]) {
+                j --;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
         return nums;
     }
 
-    public static boolean isDesc(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i ++) {
-            if (nums[i] < nums[i + 1]) {
-                return false;
-            }
+    public static void swap(int[] nums, int i, int j) {
+        int a = nums[i];
+        nums[i] = nums[j];
+        nums[j] = a;
+    }
+
+    public static void reverse(int[] nums, int i) {
+        int left = i; int right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left ++;
+            right --;
         }
-        return true;
     }
 
     public static void main(String[] args) {
