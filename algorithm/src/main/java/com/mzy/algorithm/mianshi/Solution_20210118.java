02eec5dfe904e279229c00d9140a77f05367b70d
米照雅 <mizy@wdcloud.cc>
2021-01-18 14:04:12 +0800

    外观数列

---
+++ algorithm/src/main/java/com/mzy/algorithm/mianshi/Solution_20210118.java
@@ -0,0 +1,16 @@
+package com.mzy.algorithm.mianshi;
+
+/**
+ * @author ：mizhaoya
+ * @date ：2021/1/18 14:26
+ * @description：
+ */
+public class Solution_20210118 {
+
+    public static String countAndSay(int n) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+    }
+}
