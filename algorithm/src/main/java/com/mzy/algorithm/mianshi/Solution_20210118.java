def5802bf434202e0b8b154725fddeccfc7d2199
米照雅 <mizy@wdcloud.cc>
2021-01-19 12:01:02 +0800

    外观数列

--- algorithm/src/main/java/com/mzy/algorithm/mianshi/Solution_20210118.java
+++ algorithm/src/main/java/com/mzy/algorithm/mianshi/Solution_20210118.java
@@ -8,9 +8,28 @@ package com.mzy.algorithm.mianshi;
 public class Solution_20210118 {
 
     public static String countAndSay(int n) {
-        return null;
+        if (n == 1) {
+            return "1";
+        }
+        String s = countAndSay(n - 1);
+        int start = 0;
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < s.length(); i ++) {
+            if (s.charAt(i) != s.charAt(start)) {
+                result.append(i - start).append(s.charAt(start));
+                start = i;
+            }
+        }
+        result.append(s.length() - start).append(s.charAt(start));
+        return result.toString();
     }
 
     public static void main(String[] args) {
+        System.out.println(countAndSay(1));
+        System.out.println(countAndSay(2));
+        System.out.println(countAndSay(3));
+        System.out.println(countAndSay(4));
+        System.out.println(countAndSay(5));
+        System.out.println(countAndSay(6));
     }
 }
